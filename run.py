1a4c69b523f56775feecbfa9809e1d0a3b63ee57
Fabio Matti <fabio.matti@students.unibe.ch>
2021-10-12 08:01:05 +0200

    automate prediction generation

--- run.py
+++ run.py
@@ -3,38 +3,41 @@
 Run
 ***
 
-Run this file in order to obtain the predictions of our regressors.
+Run this file in order to obtain the predictions for each of our regressors.
+The parameters are specified in 'params', and the predictions are stored in a
+.csv file in directory 'data/submission_[NAME OF THE REGRESSOR]'.
+
 """
 
-# Useful starting lines
+# Package importations
 import numpy as np
-
-
-# Loading and standardizing the training data
 from proj1_helpers import *
-y, tX, ids = load_csv_data('data/train.csv')
-tX = standardize(tX)
-
-
-# Fitting the regressors
 from implementations import *
 
-lambda_ = 0.1
-initial_w = np.ones(30, dtype=float)
-gamma = 0.1
-max_iters = 100
+# Loading and standardizing the training data
+y_train, tX_train, _ = load_csv_data('data/train.csv')
+tX_train = standardize(tX_train)
 
-weights, loss = least_squares_GD(y, tX, initial_w, max_iters, gamma)
-weights, loss = least_squares_SGD(y, tX, initial_w, max_iters, gamma)
-weights, loss = least_squares(y, tX)
-weights, loss = ridge_regression(y, tX, lambda_)
-# weights, loss = logistic_regression(y, tX, initial_w, max_iters, gamma)
-# weights, loss = reg_logistic_regression(y, tX, lambda_, initial_w, max_iters, gamma)
-
-# Loading and standardizing the test-data
+# Loading and standardizing the test data
 _, tX_test, ids_test = load_csv_data('data/test.csv')
 tX_test = standardize(tX_test)
 
-# Creating the predictions
-y_pred = predict_labels(weights, tX_test)
-create_csv_submission(ids_test, y_pred, 'data/submission.csv')
+regressors = ['least_squares_GD',
+              'least_squares_SGD',
+              'least_squares',
+              'ridge_regression',
+              'logistic_regression',
+              'reg_logistic_regression']
+
+params = [{'initial_w': np.ones(tX_train.shape[1]), 'max_iters': 100, 'gamma': 0.1},
+          {'initial_w': np.ones(tX_train.shape[1]), 'max_iters': 100, 'gamma': 0.1},
+          {},
+          {'lambda_': 0.1},
+          {'initial_w': np.ones(tX_train.shape[1]), 'max_iters': 100, 'gamma': 0.1},
+          {'lambda_': 0.1, 'initial_w': np.ones(tX_train.shape[1]), 'max_iters': 100, 'gamma': 0.1},]
+
+for regressor, param in zip(regressors, params):
+
+    weights, _ = eval(regressor)(y_train, tX_train, **param)
+    y_pred = predict_labels(weights, tX_test)
+    create_csv_submission(ids_test, y_pred, 'data/submission_' + regressor + '.csv')
