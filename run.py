24e0bedb3a3311c3f824e9c367f936c3b3a6dd36
Fabio Matti <fabio.matti@students.unibe.ch>
2021-10-19 11:28:51 +0200

    simplify run.py structure

--- run.py
+++ run.py
@@ -2,7 +2,6 @@
 """
 Run
 ***
-
 Run this file in order to obtain the predictions for each of our regressors.
 The parameters are specified in 'params', and the predictions are stored in a
 .csv file in directory 'data/submission_[NAME OF THE REGRESSOR]'.
@@ -20,47 +19,38 @@ tX_train = standardize(tX_train)
 
 # Loading and standardizing the test data
 _, tX_test, ids_test = load_csv_data('data/test.csv')
-tX_test = standardize(tX_test)
 
-# Derived data sets
-tX_test_p1234 = polynomial_basis(tX_test, [1, 2, 3, 4, 5, 6, 7, 8], std=True)
-tX_train_p1234 = polynomial_basis(tX_train, [1, 2, 3, 4, 5, 6, 7, 8], std=True)
-
-
-# Experiment configuration
-regressors = ['ridge_regression']
-
-sets = [{'tX_train' : tX_train_p1234, 'tX_test' : tX_test_p1234}]
-
-params = [{'lambda_': 1e-3}]
-
-"""
 # Final experiment configurations
-regressors = ['least_squares_GD',
-              'least_squares_SGD',
-              'least_squares',
-              'ridge_regression',
-              'logistic_regression',
-              'reg_logistic_regression']
+configs = [{'regressor' : 'least_squares_GD',
+            'degrees' : [1, 2, 3],
+            'params' : {'initial_w': None, 'max_iters': 1800, 'gamma': 0.009}},
 
-sets = [{'tX_train' : tX_train_p123456, 'tX_test' : tX_test_p123456},
-        {'tX_train' : tX_train, 'tX_test' : tX_test},
-        {'tX_train' : tX_train, 'tX_test' : tX_test},
-        {'tX_train' : tX_train_p123456, 'tX_test' : tX_test_p123456},
-        {'tX_train' : tX_train, 'tX_test' : tX_test},
-        {'tX_train' : tX_train, 'tX_test' : tX_test}]
+            {'regressor' : 'least_squares_SGD',
+            'degrees' : [1, 2, 3],
+            'params' : {'initial_w': np.ones(tX_train.shape[1]), 'max_iters': 9800, 'gamma': 0.098}},
 
-params = [{'initial_w': np.ones(tX_train.shape[1]), 'max_iters': 1800, 'gamma': 0.009},
-          {'initial_w': np.ones(tX_train.shape[1]), 'max_iters': 9800, 'gamma': 0.098},
-          {},
-          {'lambda_': 2e-5},
-          {'initial_w': np.ones(tX_train.shape[1]), 'max_iters': 100, 'gamma': 0.1},
-          {'lambda_': 0.1, 'initial_w': np.ones(tX_train.shape[1]), 'max_iters': 100, 'gamma': 0.1},]
-"""
+           {'regressor' : 'least_squares',
+            'degrees' : [1, 2, 3],
+            'params' : {}},
+
+           {'regressor' : 'ridge_regression',
+            'degrees' : [1, 2, 3],
+            'params' : {'lambda_': 1e-3}},
+
+           {'regressor' : 'logistic_regression',
+            'degrees' : [1, 2, 3],
+            'params' : {'initial_w': np.ones(tX_train.shape[1]), 'max_iters': 100, 'gamma': 0.1}},
+
+           {'regressor' : 'reg_logistic_regression',
+            'degrees' : [1, 2, 3],
+            'params' : {'lambda_': 0.1, 'initial_w': np.ones(tX_train.shape[1]), 'max_iters': 100, 'gamma': 0.1}}]
 
 
-for regressor, set, param in zip(regressors, sets, params):
+for config in configs:
 
-    weights, _ = eval(regressor)(y_train, set['tX_train'], **param)
-    y_pred = predict_labels(weights, set['tX_test'])
-    create_csv_submission(ids_test, y_pred, 'data/submission_' + regressor + '.csv')
+    tX_train_poly = polynomial_basis(tX_train, config['degrees'], std=True)
+    tX_test_poly = polynomial_basis(tX_test, config['degrees'], std=True)
+
+    weights, _ = eval(config['regressor'])(y_train, tX_train_poly, **config['params'])
+    y_pred = predict_labels(weights, tX_test_poly)
+    create_csv_submission(ids_test, y_pred, 'data/submission_' + config['regressor'] + '.csv')
