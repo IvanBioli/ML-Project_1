d67a8cd485c6c37cf4bded577924e8d60e615f63
FMatti <79205741+FMatti@users.noreply.github.com>
2021-10-28 18:00:09 +0200

    redo run filestructure

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ Run
 Run this file in order to obtain the predictions for each of our regressors.
 The parameters are specified in 'params', and the predictions are stored in a
 .csv file in directory 'data/submission_[NAME OF THE REGRESSOR]'.
-
 Minimum working example
 -----------------------
 >>> from proj1_helpers import *
@@ -30,61 +29,61 @@ y_train, tX_train, _ = load_csv_data('data/train.csv')
 # Loading the test data
 _, tX_test, ids_test = load_csv_data('data/test.csv')
 
-tX_nan_train = np.where(tX_train != -999, tX_train, np.nan)
-med_train = np.nanmedian(tX_nan_train, axis = 0)
-tX_train = np.where(~np.isnan(tX_nan_train), tX_nan_train, med_train)
+# Determine the ratio of samples with label -1 to the total samples
+pred_ratio = sum(y_train == -1) / len(y_train)
 
-tX_nan_test = np.where(tX_test != -999, tX_test, np.nan)
-med_test = np.nanmedian(tX_nan_test, axis = 0)
-tX_test = np.where(~np.isnan(tX_nan_test), tX_nan_test, med_test)
+tX_train_median = substitute_outliers(tX_train, 'median')
+tX_test_median = substitute_outliers(tX_test, 'median')
 
 configs = [{'regressor' : 'least_squares_GD',
             'degrees' : [1, 2, 3],
-            'params' : {'gamma': 8e-2, 'max_iters': 200}}]
-
-
-"""
-configs = [{'regressor' : 'least_squares_GD',
-            'degrees' : [1, 2, 3],
-            'params' : {'gamma': 8e-2, 'max_iters': 200}}]
-
-configs = [{'regressor' : 'least_squares_SGD',
-            'degrees' : [1, 2],
-            'params' : {'gamma': 3e-4, 'max_iters': 10000}}]
-
-# Final experiment configurations
-configs = [{'regressor' : 'least_squares_GD',
-            'degrees' : [1, 2, 3],
-            'params' : {'gamma': 6e-2, 'max_iters': 200}},
+            'params' : {'gamma': 9e-2, 'max_iters': 200},
+            'pred_ratio' : pred_ratio,
+            'tX_train' : tX_train_median,
+            'tX_test' : tX_test_median},
 
             {'regressor' : 'least_squares_SGD',
-            'degrees' : [1, 2, 3],
-            'params' : {'gamma': 0.0001}},
+            'degrees' : [1, 2],
+            'params' : {'gamma': 3e-4, 'max_iters': 10000, 'seed': 0},
+            'pred_ratio' : pred_ratio,
+            'tX_train' : tX_train_median,
+            'tX_test' : tX_test_median},
 
            {'regressor' : 'least_squares',
-            'degrees' : [1, 2, 3, 4, 5],
-            'params' : {}},
-
+            'degrees' : [1, 2, 3, 4],
+            'params' : {},
+            'pred_ratio' : pred_ratio,
+            'tX_train' : tX_train_median,
+            'tX_test' : tX_test_median},
+            
            {'regressor' : 'ridge_regression',
-            'degrees' : [1, 2, 3, 4, 5],
-            'params' : {'lambda_': 1e-5}},
-
-           {'regressor' : 'logistic_regression',
+            'degrees' : [1, 2, 3, 4, 5, 6, 7],
+            'params' : {'lambda_': 1e-8},
+            'pred_ratio' : pred_ratio,
+            'tX_train' : tX_train_median,
+            'tX_test' : tX_test_median},
+            
+            {'regressor' : 'logistic_regression',
             'degrees' : [0, 1, 2, 3],
-            'params' : {'gamma': 0.001}},
+            'params' : {'gamma': 0.001},
+            'pred_ratio' : False,
+            'tX_train' : tX_train_median,
+            'tX_test' : tX_test_median},
 
            {'regressor' : 'reg_logistic_regression',
             'degrees' : [0, 1],
-            'params' : {'lambda_': 0.0001}}]
-"""
+            'params' : {'lambda_': 0.0001},
+            'pred_ratio' : False,
+            'tX_train' : tX_train_median,
+            'tX_test' : tX_test_median}]
 
 for config in configs:
 
     # Raise the sets to a polynomial basis (and standardize them simultaneously)
-    tX_train_poly = polynomial_basis(tX_train, config['degrees'], std=True)
-    tX_test_poly = polynomial_basis(tX_test, config['degrees'], std=True)
+    tX_train_poly = polynomial_basis(config['tX_train'], config['degrees'], std=True)
+    tX_test_poly = polynomial_basis(config['tX_test'], config['degrees'], std=True)
 
     # Fitting the regressor configurations and creating predictions
     weights, _ = eval(config['regressor'])(y_train, tX_train_poly, **config['params'])
-    y_pred = predict_labels(weights, tX_test_poly)
+    y_pred = predict_labels(weights, tX_test_poly, config['pred_ratio'])
     create_csv_submission(ids_test, y_pred, 'data/submission_' + config['regressor'] + '.csv')
