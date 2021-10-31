77e19d752aa4c6ac62a6ae561cbb3da35edb09c0
Fabio Matti <fabio.matti@students.unibe.ch>
2021-10-31 21:27:46 +0100

    remove prediction ratio adjustment

--- proj1_helpers.py
+++ proj1_helpers.py
@@ -24,18 +24,12 @@ def load_csv_data(data_path, sub_sample=False):
     return yb, input_data, ids
 
 
-def predict_labels(weights, data, pred_ratio=False):
+def predict_labels(weights, data):
     """Generates class predictions given weights, and a test data matrix"""
     y_pred = np.dot(data, weights)
 
-    # adjust threshold value to obtain a given prediction ratio
-    if not pred_ratio:
-        threshold = 0
-    else:
-        threshold = np.quantile(y_pred, pred_ratio)
-
-    y_pred[np.where(y_pred <= threshold)] = -1
-    y_pred[np.where(y_pred > threshold)] = 1
+    y_pred[np.where(y_pred <= 0)] = -1
+    y_pred[np.where(y_pred > 0)] = 1
 
     return y_pred
 
